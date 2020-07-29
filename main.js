28866b803fa189e60e9d47b8b0fdb2837e04c73f
Aren <reactDOM.render@yandex.ru>
2020-07-29 16:31:39 +0300

    anim

--- main.js
+++ main.js
@@ -16,32 +16,3 @@ let swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
     },
   });
-
-onSetBodyColorLeft = () => {
-    const bg = document.getElementById('bg')
-    bg.style.transition = '.5s'
-    bg.style.background = 'linear-gradient(#03a9f4, #e91e63)'
-}
-onSetBodyColorCenter = () => {
-    const bg = document.getElementById('bg')
-    bg.style.transition = '.5s'
-    bg.style.background = 'linear-gradient(#ffc107, #57ff57)'
-}
-onSetBodyColorRight = () => {
-    const bg = document.getElementById('bg')
-    bg.style.transition = '.5s'
-    bg.style.background = 'linear-gradient(#ffc107, #e91e63)'
-}
-onUnSetBodyColor = () => {
-    const bg = document.getElementById('bg')
-    bg.style.background = 'transparent'
-}
-
-box1.addEventListener('mouseover', onSetBodyColorLeft);
-box2.addEventListener('mouseover', onSetBodyColorCenter);
-box3.addEventListener('mouseover', onSetBodyColorRight);
-
-box1.addEventListener('mouseout', onUnSetBodyColor);
-box2.addEventListener('mouseout', onUnSetBodyColor);
-box3.addEventListener('mouseout', onUnSetBodyColor);
-
